be8fb62f34808cfbf162ee24f3553ebc0803fea1
Keaton Taylor <keatonstaylor@gmail.com>
2017-10-20 22:43:41 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# python-spectrum-change-channel
+
+Python 3 Tool for changing the channel on your Spectrum (TWC/Brighthouse/Charter) Set-Top-Box remotely.
+
+## Prerequisites
+
+Must have Spectrum TV service and STB.
+
+## Install
+
+`pip install requests_oauthlib`
+
+## Usage
+
+```
+python3.6 changeChannel.py <username> <password> <channel> <stb-mac-address>
+```
+
+## Development
+
+### Contributions
+
+Contributions are welcome.
+
+## Disclaimer
+Not affiliated with Spectrum. Use at your own risk.
---
+++ changeChannel.py
@@ -0,0 +1,57 @@
+from requests_oauthlib import OAuth1Session
+import sys
+
+client_key = 'l7xx66025f7d4f4646b0b1cdf24a846ce1a8'
+client_secret = 'cbb3e04b32b74691b738181fdc9444e6'
+request_token_url = 'https://api.spectrum.net/auth/oauth/request'
+base_authorization_url = 'https://api.spectrum.net/auth/oauth/device/authorize'
+access_token_url = 'https://api.spectrum.net/auth/oauth/token'
+tune_channel_base_url = 'https://api.spectrum.net/ipvs/api/smarttv/epg/v0/stb/tune'
+
+def get_session(username, password):
+	## Get Token for Auth
+    oauth = OAuth1Session(client_key, client_secret=client_secret)
+    token_response = oauth.fetch_request_token(request_token_url)
+    resource_owner_key = token_response.get('oauth_token')
+    resource_owner_secret = token_response.get('oauth_token_secret')
+
+    ## Authorize Credentials
+    authorization_url = oauth.authorization_url(base_authorization_url, username=username, password=password)
+    r = oauth.get(url=authorization_url)
+    auth_response = oauth.parse_authorization_response(authorization_url + "&" + r.text)
+    token_verifier = auth_response.get('oauth_verifier')
+
+    ## Get Access Token
+    oauth = OAuth1Session(client_key,
+                          client_secret=client_secret,
+                          resource_owner_key=resource_owner_key,
+                          resource_owner_secret=resource_owner_secret,
+                          verifier=token_verifier)
+
+    oauth_tokens = oauth.fetch_access_token(access_token_url)
+    resource_owner_key = oauth_tokens.get('oauth_token')
+    resource_owner_secret = oauth_tokens.get('oauth_token_secret')
+
+    ## Generate OAuth Object with Access Token
+    oauth = OAuth1Session(client_key,
+                   client_secret=client_secret,
+                   resource_owner_key=resource_owner_key,
+                   resource_owner_secret=resource_owner_secret,
+                   verifier=token_verifier)
+
+    return oauth
+    
+def change_channel(oauth, channel, mac):
+	params = {'apicall': 'tune-channel', 'channel': channel, 'mac': mac}
+	r = oauth.get(tune_channel_base_url, params=params)
+	print(r.text)
+
+
+# Generate the session request and change the channel
+oauth = get_session(sys.argv[1], sys.argv[2])
+change_channel(oauth, sys.argv[3], sys.argv[4])
+
+
+
+
+
